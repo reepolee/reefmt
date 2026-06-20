fd5df7f1331746890716b2f494702aac7a003f21
David Somerset <alesvaupotic@icloud.com>
2026-06-20 12:04:27 +0200

    checkpoint before reefmt test loop

---
+++ test-files/output/type.ts
@@ -0,0 +1,19 @@
+    // ---------------------------------------------------------------------------
+// Types
+// ---------------------------------------------------------------------------
+
+export type ProcessOptions = {
+	/** 
+	 * Supported output formats and their MIME types. 
+	 * */
+	crop?: {
+		left: number; // my comment
+		top: number;
+		width: number;
+		height: number;
+	};
+	resize?: { width: number; height: number; };
+	format?: string;
+	quality?: number;
+	delete_original?: boolean;
+};
