0682aaf7ac7c862c14b2ecab7f6c532f348c4fcc
Aleš Vaupotič <ales@vaupotic.eu>
2026-07-08 14:07:51 +0200

    logs

--- onpull.ps1
+++ onpull.ps1
@@ -1,2 +1,5 @@
+Write-Host "Starting reefmt release process..."
 .\release.ps1
+Write-Host "Running bash release script..."
 wsl bash -c "cd /mnt/c/Users/ales/code/labs/reefmt && bash release.sh"
+Write-Host "Release process complete"
